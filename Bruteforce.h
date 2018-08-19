3e9a263e2616324fd386e3d16da05185d2af7b8f
maste98 <adilhamid98@gmail.com>
2018-08-19 18:14:55 -0500

    Add files via upload

---
+++ Bruteforce.h
@@ -0,0 +1,14 @@
+/*
+ * Bruteforce.h
+ *
+ *  Created on: Aug 19, 2018
+ *      Author: Adil
+ */
+
+#ifndef BRUTEFORCE_H_
+#define BRUTEFORCE_H_
+
+class Bruteforce {
+};
+
+#endif /* BRUTEFORCE_H_ */
